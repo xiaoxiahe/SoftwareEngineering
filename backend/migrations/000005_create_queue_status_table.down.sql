da5c2e14e5ce8364b264a4f35c6baec9a90f2878
xiaoxiahe <1627213059@qq.com>
2025-06-04 01:18:14 +0800

    更新

--- backend/migrations/000005_create_queue_status_table.down.sql
+++
@@ -1,8 +0,0 @@
--- 删除索引
-DROP INDEX IF EXISTS idx_queue_status_pile_id;
-DROP INDEX IF EXISTS idx_queue_status_user_id;
-DROP INDEX IF EXISTS idx_queue_status_request_id;
-DROP INDEX IF EXISTS idx_queue_status_entered_at;
-
--- 删除表
-DROP TABLE IF EXISTS queue_status;
--- backend/migrations/000007_create_billing_details_table.down.sql
+++
@@ -1,9 +0,0 @@
--- 删除索引
-DROP INDEX IF EXISTS idx_billing_details_user_id;
-DROP INDEX IF EXISTS idx_billing_details_session_id;
-DROP INDEX IF EXISTS idx_billing_details_generated_at;
-DROP INDEX IF EXISTS idx_billing_details_pile_id;
-DROP INDEX IF EXISTS idx_billing_details_user_generated_pile;
-
--- 删除表
-DROP TABLE IF EXISTS billing_details;
